2a1affe424dd52c4b892ad8b8ae6090a30e25436
saifhossainjibon <saifhossainjibon@gmail.com>
2026-05-22 18:29:00 +0600

    refresh token implemented

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,10 @@ import { userRoute } from "./modules/user/user.route";
 import { profileRouter } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
 import logger from "./middleware/logger";
+import CookieParser from "cookie-parser";
 
 const app: Application = express();
+app.use(CookieParser())
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import { pool } from "../../db";
-import jwt from "jsonwebtoken";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 import config from "../../config";
 const logInUserIntoDb = async (payLoad: {
   email: string;
@@ -40,6 +40,44 @@ const logInUserIntoDb = async (payLoad: {
   return {accessToken,refreshToken};
 };
 
+const genarateRefreshToken=async(token:string)=>{
+    if (!token) {
+      throw new Error("Unauthorized!!")
+    }
+    const decoded = jwt.verify(
+      token as string,
+      config.refresh_secret as string,
+    ) as JwtPayload;
+
+    const userData = await pool.query(
+      `
+        SELECT * FROM users WHERE email=$1
+        `,
+      [decoded.email],
+    );
+
+    const user = userData.rows[0];
+    if (userData.rows.length === 0) {
+      throw new Error("User Not Found!!")
+    }
+    if (!user?.is_active) {
+      throw new Error("Forbidden!!")
+    }
+
+    const jwtpayload = {
+    id: user.id,
+    name: user.name,
+    role:user.role,
+    is_active: user.is_active,
+    email: user.email,
+  };
+    // const accessToken = jwt.sign(jwtpayload, config.secret as string, { expiresIn: "1d",});
+    const accessToken = jwt.sign(jwtpayload, config.secret as string, {
+      expiresIn: "1d",
+    });
+    return {accessToken}
+}
+
 export const authService = {
-  logInUserIntoDb,
+  logInUserIntoDb,genarateRefreshToken
 };
